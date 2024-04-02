bf1f97a0bd90c2f594003d8c4954cac0cfedda4c
Gevorg Yesayan <gevorgyesayan@Gevorgs-MacBook-Air.local>
2024-04-02 16:24:47 +0400

    variables

---
+++ lesson.py
@@ -0,0 +1,159 @@
+''' LEGAL VARIABLE NAMES'''
+
+# myvar = 'my variables'
+
+# my_var = 'Gevorg'
+
+# _my_var = '55'
+
+# myVar = 55
+
+# MYVAR = 77
+
+# myvar22 = -55
+
+
+'''ILLEGAL VARIABLE NAMES '''
+
+# 3myvar = 'DAVID'
+
+# my-var = 3
+
+# last var = 4
+
+# !x = 'none'
+
+# x@ = 0
+
+'''CHI KARELI OGTAGORCEL HATUK ANUNNER'''
+
+# in = '345'
+
+# or = '789'
+
+# -----------------------------------------------------
+
+
+'''DATA TYPES'''
+
+# string-y da sovorakan toxn e
+
+# str = 'hello world'
+
+# integer - tvern e
+
+# int = 1
+
+# float - amboxjakan tver
+
+# float = 1.2
+
+# bool - True False
+
+# print(7 == 7)
+
+'''LIST-I MEJ YNDUNVACA PAHEL MIAYN MI TIPI ELEMENTNER'''
+
+# list1 = [1,2,3,4,5]
+# list-y popoxvox data type e
+
+'''TUPLE-Y POPOXVOX DATA TYPE CHE'''
+
+# tuple1 = (5, 5.1, True, 'good')
+
+# ira mej kareli e pahel mi qani tipi element
+
+# aysinq inch vor gorcoxutyun anelis listi mej erb element enq avelacnum ayn popoxvum e
+# isk tuple i mej avelacnelis ayn erku angam aveli shat hishoxutyun e zbaxecnum
+
+'''DICT- I MEJ MEK ELEMENTY UNI ERKU ARJEQ, KEY AND VALUE'''
+
+# mydict = {
+#     'car':'mercedes'
+# }
+
+'''SET- KRKNVOX ARJEQNERIC MIAYN MEKNA ARTACUM '''
+
+# myset = {1,2,3,1,2,3}
+# print(myset)
+
+''' upper(), lower(), title() haskacoxutyun'''
+
+# upper-y sarquma mecatar
+# print('hello'.upper())
+
+# lower-y sarquma poqratar
+# print('PYTHON'.lower())
+
+# title-y sarquma anvanakan aysinqn arajin tary mecatar mnacacy poqratar 
+# print('gevorg YESAYAN'.title())
+
+# millioner game
+
+# print(input('yerb e stexcvel python \na)2000 b)1991 \nc)2020 d)2010\n\t----').lower() == 'b')
+
+
+'''XNDIRNER'''
+# ---------------------
+
+# 1. Write a python program which have two input and compute the area of a rectangle.
+
+# koxm1 = float(input('erankyan ej---'))
+
+# koxm2 = float(input('erankyan ej---'))
+
+# print((koxm1 * koxm2) / 2)
+# -------------------
+
+# 2. Write a python program which have one input and compute the area of a square.
+
+# koxm = float(input('qarakusu koxm---'))
+# print(koxm * koxm)
+# -------------------
+
+# 3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес. 
+# Адрес напишите в формате, принятом в вашей стране. Никакого ввода от пользователя ваша первая программа принимать не будет, 
+# только вывод на экран и больше ничего.
+
+# print('Gevorg')
+# print('+',374)
+# -------------------
+
+# 4. Напишите программу, запрашивающую у пользователя его имя. 
+# В ответ на ввод на экране должно появиться приветствие с обращением по имени, 
+# введенному с клавиатуры ранее.
+
+# x = input('dzer anuny----')
+# print('barev dzez',x)
+
+
+# 5. Напишите программу, запрашивающую у пользователя длину и ширину комнаты. 
+# После ввода значений должен быть произведен расчет площади комнаты и выведен на экран.
+
+# x = float(input('senyaki laynutyun---'))
+# y = float(input('senyaki erkarutyun---'))
+# print(x * y)
+
+#5. Создайте программу, запрашивающую у пользователя длину и ширину садового участка в футах. 
+# Выведите на экран площадь участка в акрах.
+
+# x = float(input('senyaki laynutyun---'))
+# y = float(input('senyaki erkarutyun----'))
+# print('laynutyun--',x * 43560)
+# print('erkarutyun--',y * 43560)
+
+
+# 6. Во многих странах в стоимость стеклотары закладывается определенный депозит, чтобы стимулировать покупателей напитков сдавать пустые бу-тылки. 
+# Допустим, бутылки объемом 1 литр и меньше стоят $0,10, а бутыл - ки большего объема - $0,25.
+# Напишите программу, запрашивающую у пользователя количество бутылок каждого размера. 
+# На экране должна отобразиться сумма, которую можно выручить, если сдать всю имеющуюся посуду. Отформатируйте вывод так, 
+# чтобы сумма включала два знака после запятой и дополнялась слева символом доллара.
+
+
+# x = int(input('minchev 1 litr shsheri qanak---'))
+# y = int(input('1 litric avel taroxutyamb shsheri qanak----'))
+
+# print('kazmac gumar', (x * 0.10) + (y * 0.25),'$')
+
+
+    
