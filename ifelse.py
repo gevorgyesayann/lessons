114f56c6f8c6f10654042997eb32d14dac7a319f
gevorgyesayann <gevorgyesayan00@gmail.com>
2024-04-03 02:10:16 +0400

    if else

---
+++ ifelse.py
@@ -0,0 +1,101 @@
+'''if else paymanner en, else-y if-in hakarak paymann e'''
+
+
+# x = 4
+# if x > 5:
+#     print('very good!!!')
+# else:
+#     print('very bad!!!!')
+
+
+'''goyutyun uni nayev elif vory paymannery avelacnuma'''
+
+
+
+
+# x = 5
+# if x > 5:
+#     print('very good!!!')
+# elif x == 5:
+#     print('normal')
+# else:
+#     print('very bad!!!!')
+
+
+'''XNDIRNER'''
+# -------------------------
+
+
+# Write a Python program to calculate a dog's age in human years.
+# Note: For the first two years, a dog year is equal to 10.5 human years.
+#  After that, each dog year equals 4 human years.
+
+
+#1. dog = float(input('enter dogs age----'))
+
+# if dog == 2:
+#     print('dogs age---',dog * 10.5)
+# elif dog == 1:
+#     print('dogs age---',10.5)
+# elif dog > 2:
+#     print('dogs age---', 21 + 4 * (dog - 2))
+
+
+# 2. Write a Python program to check whether an alphabet is a vowel or consonant.
+
+# x = ('i,o,e,a,u')
+# letter = input('enter letter---')
+# if letter in x:
+#     print('vowel')
+# else:
+#     print('consonant')
+
+
+# 3. Write a Python program to check this year is leap year or no.
+
+# year = int(input('enter year---'))
+# if year % 4 == 0 and year % 400 != 0:
+#     print('leap year')
+# else:
+#     print('not leap year')
+
+
+'''inlower()- stuguma bary mecatar parunakuma te voch'''
+'''istitle()- stuguma bary sksvuma mecatarov te voch'''
+
+# name = 'Gagik'
+# if name.islower():
+#     print('poqratar')
+# elif name.istitle():
+#     print('sksvuma mecatarov')
+# else:
+#     print('mecatar e parunakum')
+
+# isalpha() -- stugum e tarer
+# isdigit() -- stugume tver
+
+
+# x = '7'
+# if x.isdigit():
+#     print('tiv e')
+# else:
+#     print('tar e')
+
+
+'''randomov xndir'''
+# import random as r
+
+# user = 0
+# pc = 0
+
+# x = int(input('enter number---'))
+# pc = r.randint(0,5)
+# if x == pc:
+#     user += 1
+#     print('pc number--',pc,  'user number--',x)
+#     print('win user')
+# else:
+#     pc += 1
+#     print('pc number--',pc,  'user number--',x)
+#     print('pc win')
+
