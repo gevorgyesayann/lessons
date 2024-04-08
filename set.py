373efa10226d3d5ff6c5c080966ae6d5242ee68d
gevorgyesayann <gevorgyesayan00@gmail.com>
2024-04-09 01:04:06 +0400

    set

---
+++ set.py
@@ -0,0 +1,40 @@
+'''SET-y chpopoxvox data type-a'''
+'''index chuni, hasce chuni, hishoxutyun chuni'''
+
+# set1 = {1,2,2,3,4,4,5,6,7,7}
+# print(set1)
+
+'''krknvox elementnery chi tpum'''
+
+# add()-- elementa avelacnum
+# set1 = {1,2,3,4}
+# set1.add(5)
+# print(set1)
+
+# discard()-- jnjuma yst elementi
+# set1 = {1,2,3,4}
+# set1.discard(2)
+# print(set1)
+
+# update()--- sety kpcnuma setin
+# set1 = {1,2,3}
+# set2 = {4,5,6}
+# set1.update(set2)
+# print(set1)
+
+# union()--- eli verjic kpcnuma u krknvoxy jnjuma, nuyn bany
+# set1 = {1,2,3}
+# set2 = {4,5,1}
+# set3 = set1.union(set2)
+# print(set3)
+
+# isdisjoint()--- stuguma erku seteri zugaher elementnery nuyn tipi en te voch
+# set1 = {1,2,'a'}
+# set2 = {5,6,'b'}
+# a = set1.isdisjoint(set2)
+# print(a)
+
+# issubset()-- eti en setna vori elementneric ka myus seti mej u et myusi mej mi qani bane avel
+# isk et mec sety kochuva issuperset()
+
+'''es sety vapshe karevor chi methodnernel animasta :)'''
