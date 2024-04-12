0e23571a5a72eb5e818d303cda3d27a38ab09b57
gevorgyesayann <gevorgyesayan00@gmail.com>
2024-04-12 13:49:00 +0400

    try except

---
+++ tryexcept.py
@@ -0,0 +1,106 @@
+'''esi try except arhestakan errori dasa'''
+# ------------------------
+
+
+'''try- aysinqn pordzi ashxatacnel ete chi ashxatum tpi excepty'''
+'''except-um nshumenq te inch sxali depquma ashxatum'''
+
+
+# try:
+#     print(y)
+# except NameError:
+#     print('chi kareli')
+
+
+# --------------------------------------------------
+
+
+'''nayev goyutyun uni else vory ashxatuma ete try-y jishta'''
+
+
+# try:
+#     print('hello')
+# except NameError:
+#     print('chi ashxatum')
+# else:
+#     print('amen inch jishta')
+
+
+# --------------------------------------------------
+
+
+'''finally-n ashxatuma yuraqanchyur depqum '''
+
+# try:
+#     print('hello')
+# except NameError:
+#     print('sxal ban ka')
+# finally:
+#     print('kody grvac e')
+
+
+# --------------------------------------------------
+
+
+'''RAISE-y jsiht grvac kodi depqum arhestakan errora stexcum'''
+
+# x = -1
+# if x < 0:
+#     raise Exception('es hamadzayn chem')
+
+
+# --------------------------------------------------
+
+
+'''ISInSTANCE-y ashxatuma ete tvyal paymany jishta'''
+
+# text = 'hello'
+
+# if isinstance(text, str):
+#     print('jishta')
+# else:
+#     print('sxala')
+
+
+'''sa kareli e ogtagorcel nayev raise-i het'''
+
+# text = 'hello'
+# if isinstance(text, str):
+#     raise TypeError('miayn tiv kareli e grel')
+
+
+
+# --------------------------------------------------
+
+
+'''python-i errorneri tesaknery'''
+
+# INDEX ERROR- inchpes stanal
+
+# list = [1,2,3,4]
+# print(list[5])
+
+# KEY ERROR- inchpes stanal
+
+# dict = {}
+# print(dict[2])
+
+# TYPE ERROR- incpes stanal 
+
+# for i in 55:
+#     print(i)
+
+# NAME ERROR- incpes stanal
+
+# x = y
+# print(x)
+
+# ZERODIVISIONERROR- inchpes stanal
+
+# print(5 / 0)
+
+# MODULENOTFOUNDERROR- inchpes stanal
+
+# import gago
+
+
