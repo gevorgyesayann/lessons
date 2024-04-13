4d3f7b03d2613b54012f0ebfe4955eb7315e5faa
gevorgyesayann <gevorgyesayan00@gmail.com>
2024-04-13 13:25:20 +0400

    modules

---
+++ modules.py
@@ -0,0 +1,69 @@
+'''modulnery linumen ereq tesaki gradaran, framework, mekel vor menqenq stexcum'''
+# def fib(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fib(n - 1) + fib(n - 2)
+# print(fib(10))
+'''heto stexcumenq urish file'''
+# modules1.py
+
+'''nuyn dzevi dict arjeq karoxenq yntex cuyc tal'''
+# dict1 = {
+#     'name':'davit',
+#     'age':12,
+#     }
+
+'''import platform - cuyca tali te incha operacion hamakargy'''
+
+# import platform
+# x = platform.system()
+# print(x)
+
+
+'''xndirner'''
+
+# 1. Create a python function factorial and import this file in another file and print factorial.
+
+# def fact(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return n * fact(n - 1)
+
+# 2. Write a Python function to calculate surface volume and area of a cylinder (glan) •
+# V=pr^2h and    A=2prh+2pr^2
+# import math
+# def func(r,h):
+#     V = math.pi * (r ** 2) * h
+#     A = 2 * (math.pi * r * h) + 2 * ( math.pi * (r ** 2))
+#     return f'glani makeresy--{A} isk cavaly---{V}'
+
+
+# 5. Write a Python function to print all primes smaller than or equal specified number.
+# Call function:numbers (9)
+# Output: (2, 3, 5, 7)
+
+# def func(n):
+#     for i in range(2, n + 1):
+#         for j in range(2,i):
+#             if i % j == 0:
+#                 break
+#         else:
+#             print(i)
+
+# 4. Write a Python function to convert degree to radian.
+# one radian = pi/180:
+# 90' radian = 1.57...
+
+# import math
+# def func(n):
+#     radian = n * math.pi / 180
+    # return radian
+    
+                   
+    
---
+++ modules1.py
@@ -0,0 +1,36 @@
+'''stex import enq anum en file-y'''
+
+# import modules
+# print(modules.fib(10))
+
+
+# age = modules.dict1['age']
+# print(age)
+# --------------------------------------
+
+
+# factoriali xndir
+# import modules
+
+# print(modules.fact(5))
+# ------------------------------------
+
+
+# matemi xndir
+
+# import modules
+# print(modules.func(2,3))
+# ----------------------------------
+
+# parz tveri xndir
+
+# import modules
+# modules.func(10)
+
+# -------------------------------------
+
+# radiani xndir
+
+# import modules
+# print(modules.func(90))
+
